3b9677751ad073900effc2fa5cbd21d903cc3e5a
Veaceslav <118342408+vCaisim@users.noreply.github.com>
2023-05-15 14:45:44 +0300

    fix issue

--- dist/index.js
+++ dist/index.js
@@ -2861,6 +2861,7 @@ const http = new HttpClient("custom-github-action", [
     }
 
     if (res.message.statusCode !== 200) {
+      core.debug(resBody.errors)
       core.setFailed(resBody.errors[0].message);
     }
 
